f4eb7f99182f20aec0a0d898053418349fe35d41
samir <samirdahal175@gmail.com>
2020-06-21 21:01:06 +0545

    pong pong particles

---
+++ PongPongUI/Particle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PongPongUI
+{
+    public class Particle
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public ConsoleColor Color { get; set; }
+    }
+}
---
+++ PongPongUI/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace PongPongUI
+{
+    class Program
+    {
+        #region particle list
+        public static List<Particle> Particles { get; private set; } = new List<Particle>();
+        #endregion
+        #region main function
+        static void Main(string[] args)
+        {
+            //disable cursor in console window
+            Console.CursorVisible = false;
+            while (true)
+            {
+                Thread.Sleep(40);
+                CreateParticle();
+                DrawParticle();
+                ClearParticle();
+            }
+        }
+        #endregion
+        #region creates specified number of particles
+        private static void CreateParticle(int count = 1)
+        {
+
+            for (int i = 0; i < count; i++)
+            {
+                Particles.Add(new Particle
+                {
+                    X = new Random().Next(Console.WindowWidth),
+                    Y = new Random().Next(Console.WindowHeight),
+                    Color = RandomColor()[new Random().Next(RandomColor().Count)]
+                });
+            }
+        }
+        #endregion
+        #region generates random colors
+        private static List<ConsoleColor> RandomColor()
+        {
+            List<ConsoleColor> Colors = new List<ConsoleColor>();
+            Colors.Add(ConsoleColor.Red);
+            Colors.Add(ConsoleColor.Green);
+            Colors.Add(ConsoleColor.Yellow);
+            Colors.Add(ConsoleColor.Magenta);
+            Colors.Add(ConsoleColor.Blue);
+            Colors.Add(ConsoleColor.Cyan);
+            return Colors;
+        }
+        #endregion
+        #region draws particle
+        private static void DrawParticle()
+        {
+            foreach (var particle in Particles)
+            {
+                Console.SetCursorPosition(particle.X, particle.Y);
+                Console.ForegroundColor = particle.Color;
+                Console.Write(".");
+            }
+        }
+        #endregion
+        #region clear particles
+        private static void ClearParticle()
+        {
+            Particles = new List<Particle>();
+        }
+        #endregion
+    }
+}
